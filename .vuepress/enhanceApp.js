b803b15841e6beb39e954dba367953af10aca54a
Steinnagel, Klaus <ksteinnagel@bna.com>
2019-04-29 10:36:58 -0400

    1.0.2

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,34 +1,13 @@
-import {Youtube, Brightcove, IFrameWrapper} from 'bnacomponents'
+import { Youtube, Brightcove, IFrameWrapper } from 'bnacomponents'
+
+const bnacomponents = require('bnacomponents');
 
 export default ({
-    Vue, 
+    Vue,
     siteData
 }) => {
-    siteData.themeConfig.sidebar = insertDynamicSidebar(siteData.themeConfig.sidebar, siteData.pages),
-    Vue.component('Youtube', Youtube),
-    Vue.component('IFrameWrapper', IFrameWrapper),
-    Vue.component('Brightcove', Brightcove)
-}
-
-function insertDynamicSidebar(sidebar, pages) {
-    return sidebar.map((item) => {
-        if (typeof item === 'string') {
-            if (item.match(/\/\*$/)) {
-                let ParentPath = item.substring(0, item.length - 1);
-                item = {
-                    title: '',
-                    children: [ParentPath]
-                };
-                pages.forEach(page => {
-                    if (page.path.startsWith(ParentPath))
-                        if (page.path === ParentPath) {
-                            item.title = "";
-                            item.collapsable = false;
-                        }
-                        else item.children.push(page.path);
-                });
-            }
-        }
-        return item;
-    });
+    siteData.themeConfig.sidebar = bnacomponents.insertDynamicSidebar(siteData.themeConfig.sidebar, siteData.pages),
+        Vue.component('Youtube', Youtube),
+        Vue.component('IFrameWrapper', IFrameWrapper),
+        Vue.component('Brightcove', Brightcove)
 }
